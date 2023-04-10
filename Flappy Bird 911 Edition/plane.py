850805b5d85929ea28b91a0a77b42cd50e240b10
Ball-slobber1 <bertrambendsen.bb@gmail.com>
2023-04-11 00:39:35 +0200

    Update plane.py

--- Flappy Bird 911 Edition/plane.py
+++ Flappy Bird 911 Edition/plane.py
@@ -1,5 +1,7 @@
 import pygame
 
+flying = False
+
 class Plane(pygame.sprite.Sprite):
     def __init__(self, width, height, screen):
         super().__init__()
@@ -8,26 +10,60 @@ class Plane(pygame.sprite.Sprite):
         self.image = pygame.Surface([width, height])
         self.image = pygame.image.load("plane.png")
         self.image = pygame.transform.scale(self.image, planesize)
-        self.velocity = 5
+        self.velocity = 0
         self.velocity1 = 8
         self.rect = self.image.get_rect()
         self.rect.x = 50
         self.rect.y = 50
-
+        self.get_pressed = False
         
     # Vi giver klassen to funktioner til bevægelse op/ned. 
     # I hver funktion opdatere vi rectens y position og tjekker at vi ikke bevæger os udover spillets ramme/skærm
+    
+    def update(self):
+        
+        self.velocity += 0.5
+        if self.velocity > 8:
+            self.velocity = 8
+        if self.rect.bottom < 500:
+            self.rect.y += int(self.velocity)
+            
+        if pygame.key.get_pressed()[pygame.K_SPACE] == 1 and self.get_pressed == False:
+            self.get_pressed = True
+            self.velocity = -10
+
+        if pygame.key.get_pressed()[pygame.K_SPACE] == 0:
+            self.get_pressed = False
+    
+        
+    
+    
     def moveUp(self):
 
-        self.rect.y -= self.velocity1
+        """if pygame.key.get_pressed()[pygame.K_SPACE] == 1 and self.get_pressed == False:
+            self.get_pressed = True
+            self.velocity = -10
+
+        if pygame.key.get_pressed()[pygame.K_SPACE] == 0:
+            self.get_pressed = False"""
+
+
+        """self.rect.y -= self.velocity1
         if self.rect.y < 0:
-            self.rect.y = 0
+            self.rect.y = 0"""
           
     def moveDown(self):
-        if self.rect.y < 0:
+        """self.velocity += 0.5
+        if self.velocity > 8:
+            self.velocity = 8
+        if self.rect.bottom < 500:
+            self.rect.y += int(self.velocity)"""
+
+
+        """if self.rect.y < 0:
             self.rect.y = 0
 
         self.rect.y += self.velocity
 
         if self.rect.y > self.screen.get_height()-self.rect.height:
-            self.rect.y = self.screen.get_height()-self.rect.height
+            self.rect.y = self.screen.get_height()-self.rect.height"""
